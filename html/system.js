5c70f82b3292b1de2ac4cb95d5065fe94d91f647
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-07-30 13:39:58 +0100

    Added basic column widths - an

--- html/system.js
+++ html/system.js
@@ -8,14 +8,17 @@ function draw(members)
         var memberEl = document.createElement("li")
 
         var nameDiv = document.createElement("div")
+        nameDiv.classList.add("name")
         nameDiv.appendChild(document.createTextNode(member["memberName"]))
         memberEl.appendChild(nameDiv)
 
         var cardDiv = document.createElement("div")
+        nameDiv.classList.add("card")
         cardDiv.appendChild(document.createTextNode(member["cardsName"]))
         memberEl.appendChild(cardDiv)
         
         var elementDiv = document.createElement("div")
+        nameDiv.classList.add("element")
         elementDiv.appendChild(document.createTextNode(member["elementName"]))
         memberEl.appendChild(elementDiv)
         
