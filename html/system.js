c0b7bf569c09ec4694bdbc334e895d6c52e889ae
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-07-30 13:15:44 +0100

    Test list of member names -an

--- html/system.js
+++ html/system.js
@@ -1,3 +1,17 @@
+
+function draw(members)
+{
+
+    var listEl = document.getElementById("members")
+
+    members.forEach(member => {
+        var memberEl = document.createElement("li")
+        var name = document.createTextNode(member["name"])
+        memberEl.appendChild(name)
+        listEl.appendChild(memberEl)
+    });
+}
+
 fetch("./memberList.json")
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) => draw(json));
