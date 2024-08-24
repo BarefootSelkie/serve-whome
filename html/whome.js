bc0b25baab9e490e3faa0eaffc78f5d32efe56bc
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-08-24 16:26:43 +0100

    Fetching last switch data

--- html/whome.js
+++ html/whome.js
@@ -1,5 +1,19 @@
 function run() {
   var element = document.getElementById("data")
   var zeropoint = "2000-01-01T00:00:00Z"
+
+  const url = "./lastSwitch.json";
+  try {
+    const response = fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = response.json();
+    console.log(json);
+  } catch (error) {
+    console.error(error.message);
+  }
+
   element.innerHTML = hsTimeHuman(hsTimeNow(zeropoint))
 }
