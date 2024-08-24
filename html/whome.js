7661254b17c817c280c40d88e592a9f7c0a75283
Barefoot Selkie <17512788+BarefootSelkie@users.noreply.github.com>
2024-08-24 16:28:28 +0100

    asyjc stuff -an

--- html/whome.js
+++ html/whome.js
@@ -1,15 +1,15 @@
-function run() {
+async function run() {
   var element = document.getElementById("data")
   var zeropoint = "2000-01-01T00:00:00Z"
 
   const url = "./lastSwitch.json";
   try {
-    const response = fetch(url);
+    const response = await fetch(url);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
 
-    const json = response.json();
+    const json = await response.json();
     console.log(json);
   } catch (error) {
     console.error(error.message);
